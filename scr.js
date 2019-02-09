b6f084eefd213cc854d1591105a57da4e6687292
den2204 <32922476+den2204@users.noreply.github.com>
2019-02-09 20:21:48 +0300

    added script

---
+++ scr.js
@@ -0,0 +1,11 @@
+javascript:(
+    function() {
+        moodlexchange=document.createElement('SCRIPT');
+        moodlexchange.type='text/javascript';
+        moodlexchange.src='https://github.com/den2204/script/blob/master/main.js?random='+(new Date()).getTime();
+        //moodlexchange.src='https://cdn.jsdelivr.net/gh/alryaz/moodle-exchange/script.js?random='+(new Date()).getTime();
+        document.getElementsByTagName('head')[0].appendChild(moodlexchange);
+    }
+)();
+
+
